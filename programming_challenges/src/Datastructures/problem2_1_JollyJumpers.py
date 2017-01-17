46e22eb3ee10a0dc0d0690c7f17b7c4699b950e1
wildmental <pbjworking@gmail.com>
2017-01-18 02:30:43 +0900

    jolly not complete but save.

--- programming_challenges/src/Datastructures/problem2_1_JollyJumpers.py
+++ programming_challenges/src/Datastructures/problem2_1_JollyJumpers.py
@@ -1,10 +1,28 @@
-'''
-Created on 2017. 1. 15.
+print(":::::Start JollyJumpers!:::::")
 
-@author: BJ Park
-'''
-print("JollyJumpers!")
-print("this is for guest to use my work")
-print("this guest branch is free from master trunk")
-print("this is for master")
-print("master branch is independent trunk")
+lines=[]
+line=[]
+def getLine():
+    while True:
+        line=input("input a sequence of numbers :").strip().split(sep=" ")
+        print(line)
+        if line==['']:
+            break
+        lines.append(line)
+
+jolly=[]checker=[]
+def judge():
+    for line in lines:
+        for i in range(1,len(line)):
+            checker.append(i)
+            gap=(int(line[i])-int(line[i-1]))>0and(int(line[i])-int(line[i-1]))or-(int(line[i])-int(line[i-1]))
+            jolly.append(gap)    
+    else:
+        lines=[]
+        line=[]
+        print("done")
+        
+        
+while 1:
+    getLine()
+    judge()
