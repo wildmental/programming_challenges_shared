b3b94b16e42f27e0e18874b2b0da7deeb3c1ea4a
wildmental <pbjworking@gmail.com>
2017-01-18 02:38:23 +0900

    middle save

--- programming_challenges/src/Datastructures/problem2_1_JollyJumpers.py
+++ programming_challenges/src/Datastructures/problem2_1_JollyJumpers.py
@@ -3,12 +3,14 @@ print(":::::Start JollyJumpers!:::::")
 lines=[]
 line=[]
 def getLine():
+    lines=[]
     while True:
         line=input("input a sequence of numbers :").strip().split(sep=" ")
         print(line)
         if line==['']:
             break
         lines.append(line)
+        print(lines)
 
 jolly=[]checker=[]
 def judge():
@@ -16,13 +18,17 @@ def judge():
         for i in range(1,len(line)):
             checker.append(i)
             gap=(int(line[i])-int(line[i-1]))>0and(int(line[i])-int(line[i-1]))or-(int(line[i])-int(line[i-1]))
-            jolly.append(gap)    
+            jolly.append(gap)
+        if sorted(jolly)==checker:
+            print("jolly")
+        else:
+            print("not jolly")    
     else:
         lines=[]
         line=[]
         print("done")
-        
-        
+                
 while 1:
     getLine()
+    print(lines)
     judge()
