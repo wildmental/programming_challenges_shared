90e589ec087e2a6848c268618bd960f1b95a1bcb
wildmental <pbjworking@gmail.com>
2017-01-18 16:53:04 +0900

    code rearrange and checked the function

--- programming_challenges/src/Datastructures/problem2_1_JollyJumpers.py
+++ programming_challenges/src/Datastructures/problem2_1_JollyJumpers.py
@@ -1,34 +1,36 @@
 print(":::::Start JollyJumpers!:::::")
 
-lines=[]
-line=[]
 def getLine():
+    line=[]
     lines=[]
     while True:
         line=input("input a sequence of numbers :").strip().split(sep=" ")
-        print(line)
+        #print(line)
         if line==['']:
-            break
+            return lines
         lines.append(line)
-        print(lines)
+        #print(lines)
 
-jolly=[]checker=[]
-def judge():
+def judge(lines):
     for line in lines:
+        jolly=[]
+        checker=[]
         for i in range(1,len(line)):
             checker.append(i)
             gap=(int(line[i])-int(line[i-1]))>0and(int(line[i])-int(line[i-1]))or-(int(line[i])-int(line[i-1]))
             jolly.append(gap)
         if sorted(jolly)==checker:
+            #print(checker, jolly)
             print("jolly")
         else:
-            print("not jolly")    
+            #print(checker, jolly)
+            print("not jolly")
     else:
         lines=[]
         line=[]
         print("done")
-                
+
 while 1:
-    getLine()
-    print(lines)
-    judge()
+    lines=getLine()
+    #print(lines)
+    judge(lines)
